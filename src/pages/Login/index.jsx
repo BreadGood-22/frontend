7c93287c837bb9eb1cd5fc345958e54067568b5e
sandwe <preengus@gmail.com>
2022-12-14 16:29:27 +0900

    design: 로그인 페이지 UI 구현 #13

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,23 @@
+import * as S from './style';
+import { Label, EmailInput, PasswordInput, LargeButton } from '../../components';
+
 export function Login() {
-  return <div>Login</div>;
+  return (
+    <S.Wrapper>
+      <S.H2>로그인</S.H2>
+      <form>
+        <S.EmailWrapper>
+          <Label>이메일</Label>
+          <EmailInput />
+        </S.EmailWrapper>
+        <Label>비밀번호</Label>
+        <PasswordInput />
+        <S.WarningText isVisible={false}>*이메일 또는 비밀번호가 일치하지 않습니다.</S.WarningText>
+        <S.ButtonWrapper>
+          <LargeButton content='로그인' isActive={true}></LargeButton>
+        </S.ButtonWrapper>
+      </form>
+      <S.SignupLink to='/signup'>이메일로 회원가입</S.SignupLink>
+    </S.Wrapper>
+  );
 }
--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -1,1 +1,41 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Wrapper = styled.section`
+  width: calc(100% - 68px);
+  text-align: center;
+  margin: 0 auto;
+`;
+
+export const H2 = styled.h2`
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 30px;
+  margin: 30px 0 40px;
+`;
+
+export const EmailWrapper = styled.div`
+  margin-bottom: 16px;
+`;
+
+export const WarningText = styled.strong`
+  display: ${({ isVisible }) => (isVisible ? 'block' : 'none')};
+  color: ${({ theme }) => theme.palette.brown};
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14px;
+  text-align: left;
+  margin-top: 6px;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin: 30px 0 20px;
+`;
+
+export const SignupLink = styled(Link)`
+  color: ${({ theme }) => theme.palette.darkGray};
+  display: inline-block;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+`;
