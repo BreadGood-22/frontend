25e169e215915bab974949ecc214e9dab12d821b
sandwe <preengus@gmail.com>
2022-12-18 13:53:52 +0900

    feat: 이메일, 비밀번호 모두 입력 시 버튼 활성화되도록 구현 #26

--- src/components/login/LoginForm/index.jsx
+++ src/components/login/LoginForm/index.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import * as S from './style';
 import { Label, EmailInput, PasswordInput, LargeButton } from '../../index';
 
 export function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    setError('');
+    if (email && password) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [email, password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    /** POST 보내기
+     *
+     * */
+
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <>
-      <S.Form>
+      <S.Form onSubmit={handleSubmit}>
         <Label>이메일</Label>
-        <EmailInput required />
+        <EmailInput onChange={(e) => setEmail(e.target.value)} required />
         <Label>비밀번호</Label>
-        <PasswordInput />
-        <S.WarningText isVisible={false}></S.WarningText>
-        <LargeButton disabled={true}>로그인</LargeButton>
+        <PasswordInput onChange={(e) => setPassword(e.target.value)} required />
+        <S.WarningText isVisible={false}>{error}</S.WarningText>
+        <LargeButton disabled={isDisabled}>로그인</LargeButton>
       </S.Form>
       <S.SignupLink to='/signup'>이메일로 회원가입</S.SignupLink>
     </>
