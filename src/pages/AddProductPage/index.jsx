dc99b0ec3f41a5dee5d88ae3a9a53db9971c376a
seona.bang <sit.or.seona@gmail.com>
2022-12-22 16:26:47 +0900

    design: 상품 등록 페이지 input 유효성 검사 및 통과시 버튼 활성화 구현 #42

--- src/pages/AddProductPage/index.jsx
+++ src/pages/AddProductPage/index.jsx
@@ -1,9 +1,8 @@
-import { HeaderSave, AddProductForm } from '../../components';
+import { AddProductForm } from '../../components';
 
 export function AddProductPage() {
   return (
     <>
-      <HeaderSave />
       <AddProductForm />
     </>
   );
