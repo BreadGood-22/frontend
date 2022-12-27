a5220a5497cba23ad01421f95909c165c1a68d47
sandwe <preengus@gmail.com>
2022-12-27 09:43:06 +0900

    feat: 피드 무한 스크롤 구현 #62

---
+++ src/hooks/useIntersect.js
@@ -0,0 +1,24 @@
+import { useRef, useCallback, useEffect } from 'react';
+
+export const useIntersect = (onIntersect, options = { threshold: 1 }) => {
+  const ref = useRef(null);
+
+  const callback = useCallback(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) onIntersect(entry, observer);
+      });
+    },
+    [onIntersect],
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(callback, options);
+
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [ref, options, callback]);
+
+  return ref;
+};
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { AuthProvider } from './context/AuthProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <AuthProvider>
+    <App />
+  </AuthProvider>,
+  // </React.StrictMode>,
 );
