6acf4e79088cecb60e7f1a61c9938a806055bb6a
sandwe <preengus@gmail.com>
2022-12-27 17:00:37 +0900

    fix: 프로필 페이지 PostsContainer 높이에 따라 padding bottom 늘어나도록 수정 #70

--- src/components/profile/PostsContainer/style.js
+++ src/components/profile/PostsContainer/style.js
@@ -6,7 +6,6 @@ import PostGalleryIconOff from '../../../assets/icons/icon-post-gallery-off.svg'
 
 export const Container = styled.section`
   background-color: ${({ theme }) => theme.palette.white};
-  padding-bottom: 60px;
 `;
 
 export const TabContainer = styled.div`
