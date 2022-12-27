0ab85a0e20024e3e0a83193493332fe42e91ae2a
seona.bang <sit.or.seona@gmail.com>
2022-12-27 17:48:03 +0900

    design: 채팅방 페이지 대화 입력창 UI 구현 #68

---
+++ src/components/chatRoom/ChatInput/style.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { SmallImgButtonLabel, SmallImgButtonInput } from '../../common/Button/style';
+
+export const Container = styled.footer`
+  position: fixed;
+  bottom: 0;
+  z-index: 10;
+  width: 100%;
+  max-width: 390px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 0 16px;
+  border-top: 1px solid ${({ theme }) => theme.palette.lightGray};
+`;
+
+export const ImageLabel = styled(SmallImgButtonLabel)`
+  width: 36px;
+  height: 36px;
+  display: inline-block;
+  &::after {
+    right: 0;
+    bottom: 0;
+  }
+`;
+
+export const ImageInput = styled(SmallImgButtonInput)``;
+
+export const TextInput = styled.input.attrs({
+  type: 'text',
+  placeholder: '메시지 입력하기...',
+})`
+  flex-grow: 1;
+  border: none;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.palette.mediumGray};
+    font-size: 14px;
+    line-height: 18px;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  font-size: 14px;
+  line-height: 18px;
+  color: ${({ theme }) => theme.palette.brown};
+  cursor: pointer;
+
+  &:disabled {
+    cursor: default;
+    color: ${({ theme }) => theme.palette.mediumGray};
+  }
+`;
