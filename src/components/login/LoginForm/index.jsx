39b97c330ab8d41c21d9ddc7f6dc2de8846df28c
sandwe <preengus@gmail.com>
2022-12-28 11:05:04 +0900

    refactor: 로그인 폼에 react-hook-form 적용 #72

--- src/components/login/LoginForm/index.jsx
+++ src/components/login/LoginForm/index.jsx
@@ -1,28 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import axios from '../../../api/apiController';
 import * as S from './style';
 import { Label, EmailInput, PasswordInput, LargeButton } from '../../index';
 
 export function LoginForm() {
   const navigate = useNavigate();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [isDisabled, setIsDisabled] = useState(true);
 
-  useEffect(() => {
-    setError('');
-    if (email && password) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true);
-    }
-  }, [email, password]);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { isValid, isSubmitting },
+  } = useForm({ mode: 'onSubmit' });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleLogin = async (e) => {
+    const { email, password } = watch();
+
     const { data } = await axios.post('/user/login', {
       user: {
         email,
@@ -37,20 +34,27 @@ export function LoginForm() {
     localStorage.setItem('token', JSON.stringify(data.user?.token));
     localStorage.setItem('accountname', JSON.stringify(data.user?.accountname));
     navigate('/', { replace: true });
-
-    setEmail('');
-    setPassword('');
   };
 
   return (
     <>
-      <S.Form onSubmit={handleSubmit}>
+      <S.Form onSubmit={handleSubmit(handleLogin)}>
         <Label>이메일</Label>
-        <EmailInput onChange={(e) => setEmail(e.target.value)} required />
+        <EmailInput
+          {...register('email', {
+            required: true,
+            onChange: (e) => setValue(e.target.value),
+          })}
+        />
         <Label>비밀번호</Label>
-        <PasswordInput onChange={(e) => setPassword(e.target.value)} required />
-        <S.WarningText isVisible={!!error}>{error}</S.WarningText>
-        <LargeButton disabled={isDisabled}>로그인</LargeButton>
+        <PasswordInput
+          {...register('password', {
+            required: true,
+            onChange: (e) => setValue(e.target.value),
+          })}
+        />
+        <S.WarningText isVisible={!!error}>*{error}</S.WarningText>
+        <LargeButton disabled={!isValid || isSubmitting}>로그인</LargeButton>
       </S.Form>
       <S.SignupLink to='/signup'>이메일로 회원가입</S.SignupLink>
     </>
