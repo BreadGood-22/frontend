b28f5029353786fed682f3ef747f99cabe4dbc32
seona.bang <sit.or.seona@gmail.com>
2022-12-29 10:50:10 +0900

    refactor: 채팅방 페이지 대화 본문 영역 데이터를 분리해 코드 구조 개선 #68

--- src/components/chatRoom/ChatContents/index.jsx
+++ src/components/chatRoom/ChatContents/index.jsx
@@ -1,30 +1,53 @@
 import * as S from './style';
 
 export function ChatContents() {
+  const chatData = [
+    {
+      author: 'you',
+      textMessage: '빵굿이 굿즈 이번엔 안 만드시나요!!!!! 지난 번 마켓 때 놓쳤는데 계속 생각나요 😭',
+      imageMessage: '',
+      date: '22:17',
+    },
+    {
+      author: 'you',
+      textMessage: '',
+      imageMessage: 'imageExample',
+      date: '22:18',
+    },
+    {
+      author: 'me',
+      textMessage: '지금 준비 중에 있습니다! 3월에 빵굿이 굿즈 안내가 나갈 예정이에요!',
+      imageMessage: '',
+      date: '22:33',
+    },
+    {
+      author: 'me',
+      textMessage: '관심 가져주셔서 고맙습니다! 🥰',
+      imageMessage: '',
+      date: '22:33',
+    },
+  ];
+
   return (
     <main>
       <h2 className='sr-only'>대화 본문</h2>
       <S.ChatContents>
-        <S.YourMessageItem>
-          <S.YourProfileImage />
-          <S.YourTextMessage>
-            빵굿이 굿즈 이번엔 안 만드시나요!!!!! 지난 번 마켓 때 놓쳤는데 계속 생각나요 😭
-          </S.YourTextMessage>
-          <S.Date>22:17</S.Date>
-        </S.YourMessageItem>
-        <S.YourMessageItem>
-          <S.YourProfileImage />
-          <S.ImageMessage src='' alt='' />
-          <S.Date>22:18</S.Date>
-        </S.YourMessageItem>
-        <S.MyMessageItem>
-          <S.MyTextMessage>지금 준비 중에 있습니다! 3월에 빵굿이 굿즈 안내가 나갈 예정이에요!</S.MyTextMessage>
-          <S.Date>22:33</S.Date>
-        </S.MyMessageItem>
-        <S.MyMessageItem>
-          <S.MyTextMessage>관심 가져주셔서 고맙습니다! 🥰</S.MyTextMessage>
-          <S.Date>22:33</S.Date>
-        </S.MyMessageItem>
+        {chatData.map((item) =>
+          item.author === 'me' ? (
+            <S.MyMessageItem>
+              {item.textMessage && <S.MyTextMessage>{item.textMessage}</S.MyTextMessage>}
+              {item.imageMessage && <S.ImageMessage />}
+              <S.Date>{item.date}</S.Date>
+            </S.MyMessageItem>
+          ) : (
+            <S.YourMessageItem>
+              <S.YourProfileImage />
+              {item.textMessage && <S.YourTextMessage>{item.textMessage}</S.YourTextMessage>}
+              {item.imageMessage && <S.ImageMessage />}
+              <S.Date>{item.date}</S.Date>
+            </S.YourMessageItem>
+          ),
+        )}
       </S.ChatContents>
     </main>
   );
