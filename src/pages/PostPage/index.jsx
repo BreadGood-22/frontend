fcdbcc3c06fc3d907ec69aeca3d742ea4e7ea429
sandwe <preengus@gmail.com>
2023-01-02 17:28:57 +0900

    fix: 게시글, 댓글에서 useHeight 커스텀 훅 import하도록 수정 #124

--- src/pages/PostPage/index.jsx
+++ src/pages/PostPage/index.jsx
@@ -1,17 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { axiosPrivate } from '../../api/apiController';
 import { HeaderBasic, HeaderBasicModal, PostContainer, CommentList, CommentInput } from '../../components';
 
 export function PostPage() {
-  const container = useRef(null);
-
   const { postId: id } = useParams();
 
   const [postId, setPostId] = useState(id);
   const [post, setPost] = useState('');
   const [comments, setComments] = useState([]);
-  const [height, setHeight] = useState(0);
   const [isVisibleModal, setIsVisibleModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -48,25 +45,13 @@ export function PostPage() {
     getComments();
   }, []);
 
-  useEffect(() => {
-    if (!container.current) return;
-    setHeight(container.current.clientHeight);
-  }, [comments]);
-
   return (
     <>
-      <section ref={container}>
+      <section>
         <h2 className='sr-only'>게시글 페이지</h2>
         <HeaderBasic setIsVisibleModal={setIsVisibleModal} />
         <PostContainer postId={postId} setPostId={setPostId} post={post} getUserPost={getUserPost} />
-        <CommentList
-          height={height}
-          postId={postId}
-          getComments={getComments}
-          comments={comments}
-          post={post}
-          setPost={setPost}
-        />
+        <CommentList postId={postId} getComments={getComments} comments={comments} post={post} setPost={setPost} />
       </section>
       <CommentInput getComments={getComments} post={post} setPost={setPost} postId={postId} />
       {isVisibleModal && <HeaderBasicModal setIsVisibleModal={setIsVisibleModal} />}
