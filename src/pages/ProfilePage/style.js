4729a40ee8b322a41eba42e48e0737cc26cb5224
seona.bang <sit.or.seona@gmail.com>
2023-01-03 13:49:39 +0900

    design: 프로필 페이지 마크업 수정

--- src/pages/ProfilePage/style.js
+++ src/pages/ProfilePage/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.main`
+export const Container = styled.section`
   min-height: 100vh;
   background-color: ${({ theme }) => theme.palette.whitishGray};
 `;
