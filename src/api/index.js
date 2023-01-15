bbdc0ca59086016e3c06912d943a8fc60d1c4fad
hayeonii <hayexn51@gmail.com>
2023-01-15 17:34:36 +0900

    refactor: 유저 정보 가져오는 API 분리 #151

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,4 @@ export { addImage } from './imgUpload/addImage';
 export { addAccountNameValid } from './signup/addAccountNameValid';
 export { addUserInfo } from './signup/addUserInfo';
 export { addEmailValid } from './signup/addEmailValid';
+export { getUserInfo } from './profile/getUserInfo';
