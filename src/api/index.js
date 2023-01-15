7b40bd7f67a1f5d7976548c4a45f80e40267ae38
hayeonii <hayexn51@gmail.com>
2023-01-15 18:04:08 +0900

    refactor: 포스트 업로드 기능 API 분리 #151

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,4 @@ export { addAccountNameValid } from './signup/addAccountNameValid';
 export { addUserInfo } from './signup/addUserInfo';
 export { addEmailValid } from './signup/addEmailValid';
 export { getUserInfo } from './profile/getUserInfo';
+export { addPost } from './post/addPost';
