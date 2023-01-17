34d2a1aa0383a95e3335cfde0bbbdfaa10a081ea
hayeonii <hayexn51@gmail.com>
2023-01-17 19:07:42 +0900

    fix: 포스트 수정 시 textarea 높이값 인식하지 못하는 현상 해결 #160

--- src/components/postEdit/PostEditForm/index.jsx
+++ src/components/postEdit/PostEditForm/index.jsx
@@ -54,11 +54,18 @@ export function PostEditForm() {
     return <S.ProfileImg src={profileImage} />;
   };
 
-  const handleTextArea = (e) => {
-    textRef.current.style.height = 'auto';
-    textRef.current.style.height = `${textRef.current.scrollHeight}px`;
+  const handleText = (e) => {
     setText(e.target.value);
   };
+
+  const handleTextArea = () => {
+    textRef.current.style.height = 'auto';
+    textRef.current.style.height = `${textRef.current.scrollHeight}px`;
+  };
+
+  useEffect(() => {
+    handleTextArea();
+  }, [text]);
 
   const handleGetImageUrl = async (e) => {
     if (postImg.length < MAX_UPLOAD) {
@@ -102,7 +109,7 @@ export function PostEditForm() {
         <S.PostWrite>
           <h3 className='sr-only'>게시글 작성 form</h3>
           <S.Form>
-            <S.ContentInput onInput={handleTextArea} ref={textRef} defaultValue={text} />
+            <S.ContentInput onInput={handleText} ref={textRef} defaultValue={text} />
             <S.ImgUploadButton onChange={handleGetImageUrl}>
               <h4 className='sr-only'>이미지 업로드 버튼</h4>
               <MediumImgButton />
