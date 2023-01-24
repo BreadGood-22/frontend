04daf84a65ed55899bd38e54f1bd283f7d94e30d
seona.bang <sit.or.seona@gmail.com>
2023-01-24 21:49:50 +0900

    design: 프로필 수정 페이지 UI 구현 #172

--- src/pages/ProfileEditPage/index.jsx
+++ src/pages/ProfileEditPage/index.jsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
+import { ProfileForm, HeaderSave } from '../../components';
 
 export function ProfileEditPage() {
-  return <div>ProfileEditPage</div>;
+  const [isValid, setIsValid] = useState(false);
+  const [isProfileEdit, setIsProfileEdit] = useState(true);
+
+  return (
+    <section>
+      <h2 className='sr-only'>프로필 수정</h2>
+      <HeaderSave disabled={!isValid} formId='profile-form' />
+      <ProfileForm setIsValid={isValid} isProfileEdit={isProfileEdit} />
+    </section>
+  );
 }
