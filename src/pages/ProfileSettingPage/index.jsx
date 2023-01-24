35f3db76c0474e351eed0bb66f556e9fc8a0ce88
seona.bang <sit.or.seona@gmail.com>
2023-01-24 19:06:30 +0900

    style: 프로필 설정 페이지 주석 삭제 및 헤더 컴포넌트 통합 #172

--- src/pages/ProfileSettingPage/index.jsx
+++ src/pages/ProfileSettingPage/index.jsx
@@ -1,23 +1,12 @@
 import * as S from './style';
-import { ProfileForm, ProfileSettingButton } from '../../components/profileSetting/ProfileForm';
+import { ProfileForm } from '../../components';
 
 export function ProfileSettingPage() {
-  // 유효성 검사 최종 확인 state
-  // 그렇다면 나중에 유저 정보는 어떻게 가지고 있을 것인가?
-
-  return (
-    <S.Container>
-      <ProfileHeader />
-      <ProfileForm />
-      {/* <ProfileSettingButton /> */}
-    </S.Container>
-  );
-}
-export function ProfileHeader() {
   return (
     <S.Container>
       <S.H2>프로필 설정</S.H2>
       <S.Notice>나중에 언제든지 변경할 수 있습니다.</S.Notice>
+      <ProfileForm />
     </S.Container>
   );
 }
