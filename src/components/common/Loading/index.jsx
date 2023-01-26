e051432175ba2fbfbb4bb9fbbd144cd55082324c
sandwe <preengus@gmail.com>
2023-01-27 00:12:02 +0900

    feat: 로딩중 컴포넌트 CSS 추가 #173

--- src/components/common/Loading/index.jsx
+++ src/components/common/Loading/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import Logo from '../../../assets/images/symbol-logo.webp';
+
 import * as S from './style';
 
 export function Loading() {
-  return <S.Container>로딩중...</S.Container>;
+  return (
+    <S.Container>
+      <S.Image src={Logo} alt='빵굿이' />
+      <S.Text>로딩중...</S.Text>
+    </S.Container>
+  );
 }
