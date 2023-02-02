971c8903d521ae98398db9e499888d2fdac85a14
seona.bang <sit.or.seona@gmail.com>
2023-02-02 21:49:50 +0900

    fix: 프로필 설정/수정 페이지 로딩 컴포넌트 적용 및 불필요한 로딩 State 삭제 #180

--- src/pages/ProfileSettingPage/index.jsx
+++ src/pages/ProfileSettingPage/index.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import * as S from './style';
-import { ProfileForm } from '../../components';
+import { Loading, ProfileForm } from '../../components';
 
 export function ProfileSettingPage() {
   const [isValid, setIsValid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <S.Container>
-      <S.H2>프로필 설정</S.H2>
-      <S.Notice>나중에 언제든지 변경할 수 있습니다.</S.Notice>
-      <ProfileForm setIsValid={setIsValid} />
-      <S.Button disabled={!isValid} form='profile-form'>
-        빵굿빵굿 시작하기
-      </S.Button>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <>
+          <S.H2>프로필 설정</S.H2>
+          <S.Notice>나중에 언제든지 변경할 수 있습니다.</S.Notice>
+          <ProfileForm setIsValid={setIsValid} setIsLoading={setIsLoading} />
+          <S.Button disabled={!isValid} form='profile-form'>
+            빵굿빵굿 시작하기
+          </S.Button>
+        </>
+      )}
     </S.Container>
   );
 }
